da070e295869eb129132bd2e39cab4dd741c81a3
zhushen <60245376+zhushen12580@users.noreply.github.com>
2023-07-19 03:38:20 +0000

    modified:   app.py 	modified:   templates/index.html

--- app.py
+++ app.py
@@ -10,10 +10,8 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your-secret-key'  # change this to your secret key
-#openai.api_key = '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'  # change this to your OpenAI API key
-# 从环境变量中获取 OpenAI API 密钥
-openai.api_key = os.getenv('KEY')
-token = os.getenv('TOKEN')
+openai.api_key = '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'
+token = 'WyAsI2Zt0_tDshEfS95ccg'
 
 
 class URLForm(FlaskForm):
@@ -32,23 +30,33 @@ def index():
         urls = form.url.data.splitlines()#
         reviews = []
         for url in urls:
-            params = {
-                'token': token,
-                'scraper': 'amazon-product-reviews',
-                'format': 'json',
-                'url': url,
-            }
-            response = requests.get('https://api.crawlbase.com/', params=params)
-            data = response.json()
-            if 'body' in data and 'reviews' in data['body']:
-                reviews.extend(item['reviewText'] for item in data['body']['reviews'])
+            asin_start_index = url.find('/dp/') + 4
+            asin_end_index = url.find('/', asin_start_index)
+            asin = url[asin_start_index:asin_end_index]
+            page_number = 1
+            
+            while page_number<6:
+                url_detail = f"https://www.amazon.com/product-reviews/{asin}/ref=cm_cr_arp_d_paging_btm_next_{page_number}?ie=UTF8&reviewerType=all_reviews&pageNumber={page_number}"
+                params = {
+                    'token': token,
+                    'scraper': 'amazon-product-reviews',
+                    'format': 'json',
+                    'url': url_detail,
+                }
+                response = requests.get('https://api.crawlbase.com/', params=params)
+                data = response.json()
+                if 'body' in data and 'reviews' in data['body']:
+                    reviews.extend(item['reviewText'] for item in data['body']['reviews'])
+                if data['body']['pagination']['nextPage']=='null':
+                    break
+                page_number += 1
 
         reviews_text = '\n'.join(reviews)
 
         # 使用 OpenAI ChatGPT 处理评论文本
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
-            messages=[{"role": "system", "content": "作为nlp算法模型，分析以下产品信息及评论，找出该产品的用户需求点，格式要求：以分析报告的格式输出且是markdown格式的，层次清晰，重点突出，包含h1标题（产品设计优化方向分析报告）、小字体产品名（简称）、h2摘要、h2主要发现（每条发现标题加粗），主要发现下面增加一条引用的评论、h2结论与建议；产品信息及评论如下："},
+            messages=[{"role": "system", "content": "作为nlp算法模型，分析以下产品信息及评论，找出该产品的用户需求点及产品优化空间，格式要求：以分析报告的格式输出且是markdown格式的，层次清晰，简洁明要，包含h1标题（产品设计优化方向分析报告）、小字体产品名（简称）、h2摘要、h2主要发现（每条发现标题加粗），主要发现下面增加一条引用的评论、h2结论与建议；产品信息及评论如下："},
                       {"role": "user", "content": reviews_text}]
         )
 
