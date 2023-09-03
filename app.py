771090d8bf342440c22f999816dda2248ab9d444
zhushen <60245376+zhushen12580@users.noreply.github.com>
2023-09-03 09:23:58 +0000

    modified:   app.py

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ import requests
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your-secret-key'  # change this to your secret key
 openai.api_key = os.getenv('KEY')
+openai.api_base = "https://api.aigc369.com/v1"
 token = os.getenv('TOKEN')
 
 
@@ -130,14 +131,20 @@ def index():
             "messages": [{"role": "user", "content": "作为nlp算法模型，分析以下产品信息及评论，找出该产品的用户需求点，格式要求：以分析报告的格式输出且是markdown格式的，层次清晰，重点突出，包含h1标题（产品设计优化方向分析报告）、小字体产品名（简称）、h2摘要、h2主要发现（每条发现标题加粗），主要发现下面增加一条引用的评论,并指出有多少条相似观点、h2结论与建议；产品信息及评论如下：产品信息："+reviews_text}],
         }
         print(reviews_text)
-        response = requests.post(url, json=data, headers=headers,timeout=1200)
-
-        if response.status_code == 200:
-            result = response.json()
-            results = result["choices"][0]["message"]["content"]
-            print(result)
+        if len(reviews_text)<6000:
+            response = requests.post(url, json=data, headers=headers,timeout=1200)
+            if response.status_code == 200:
+                result = response.json()
+                results = result["choices"][0]["message"]["content"]
+                print(result)
+            else:
+                print("请求失败:", response.status_code, response.text)
         else:
-            print("请求失败:", response.status_code, response.text)
+            chat_completion = openai.ChatCompletion.create(
+            model="gpt-4-32k", messages=[{"role": "user", "content": "作为nlp算法模型，分析以下产品信息及评论，找出该产品的用户需求点，格式要求：以分析报告的格式输出且是markdown格式的，层次清晰，重点突出，包含h1标题（产品设计优化方向分析报告）、小字体产品名（简称）、h2摘要、h2主要发现（每条发现标题加粗），主要发现下面增加一条引用的评论,并指出有多少条相似观点、h2结论与建议；产品信息及评论如下：产品信息："+reviews_text}])
+            result = chat_completion.choices[0]['message']['content']
+            print(result)
+
     return render_template('index.html', form=form, results=results,stars=product_review_top)
 
 
