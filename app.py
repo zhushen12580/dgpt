7ef36d010b18b4e2df06f509a2087013431edbe4
zhushen <60245376+zhushen12580@users.noreply.github.com>
2023-09-12 18:28:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -144,8 +144,8 @@ def index():
             "messages": [{"role": "user", "content": "作为nlp算法模型，分析以下产品信息及评论，找出该产品的用户需求点，格式要求：以分析报告的格式输出且是markdown格式的，层次清晰，重点突出，包含h1标题（产品设计优化方向分析报告）、小字体产品名（简称）、h2摘要、h2主要发现（每条发现标题加粗），主要发现下面增加一条引用的评论,并指出有多少条相似观点、h2结论与建议；产品信息及评论如下：产品信息："+reviews_text}],
         }
         print(reviews_text)
-        if len(reviews_text)<6000:
-            response = requests.post(url, json=data, headers=headers,timeout=1200)
+        if len(reviews_text)<5900:
+            response = requests.post(url, json=data, headers=headers,timeout=4200)
             if response.status_code == 200:
                 result = response.json()
                 results = result["choices"][0]["message"]["content"]
