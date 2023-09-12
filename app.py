9d04905c09f5aae873921f04d76e88486a666433
zhushen <60245376+zhushen12580@users.noreply.github.com>
2023-09-12 18:37:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -111,20 +111,20 @@ def index():
                     reviews.extend(item['reviewText'] for item in data['body']['reviews'] if (len(item['reviewText']) > 36 & len(item['reviewText'])<1000))
             except:
                 continue
-            try:
-                url_detail1 = f"https://www.amazon.com/product-reviews/{asin}/ref=cm_cr_unknown?ie=UTF8&reviewerType=all_reviews&filterByStar=four_star&pageNumber=2"
-                params = {
-                    'token': token,
-                    'scraper': 'amazon-product-reviews',
-                    'format': 'json',
-                    'url': url_detail1,
-                }
-                response = requests.get('https://api.crawlbase.com/', params=params)
-                data = response.json()
-                if 'body' in data and 'reviews' in data['body']:
-                    reviews.extend(item['reviewText'] for item in data['body']['reviews'] if (len(item['reviewText']) > 36 & len(item['reviewText'])<1000))
-            except:
-                continue
+            # try:
+            #     url_detail1 = f"https://www.amazon.com/product-reviews/{asin}/ref=cm_cr_unknown?ie=UTF8&reviewerType=all_reviews&filterByStar=four_star&pageNumber=2"
+            #     params = {
+            #         'token': token,
+            #         'scraper': 'amazon-product-reviews',
+            #         'format': 'json',
+            #         'url': url_detail1,
+            #     }
+            #     response = requests.get('https://api.crawlbase.com/', params=params)
+            #     data = response.json()
+            #     if 'body' in data and 'reviews' in data['body']:
+            #         reviews.extend(item['reviewText'] for item in data['body']['reviews'] if (len(item['reviewText']) > 36 & len(item['reviewText'])<1000))
+            # except:
+            #     continue
             if 'body' in data and 'productReviewTop' in data['body']:
                 product_review_top = data['body']['productReviewTop']
                 print(product_review_top)
@@ -144,7 +144,7 @@ def index():
             "messages": [{"role": "user", "content": "作为nlp算法模型，分析以下产品信息及评论，找出该产品的用户需求点，格式要求：以分析报告的格式输出且是markdown格式的，层次清晰，重点突出，包含h1标题（产品设计优化方向分析报告）、小字体产品名（简称）、h2摘要、h2主要发现（每条发现标题加粗），主要发现下面增加一条引用的评论,并指出有多少条相似观点、h2结论与建议；产品信息及评论如下：产品信息："+reviews_text}],
         }
         print(reviews_text)
-        if len(reviews_text)<5900:
+        if len(reviews_text)<6500:
             response = requests.post(url, json=data, headers=headers,timeout=4200)
             if response.status_code == 200:
                 result = response.json()
