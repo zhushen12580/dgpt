998a33ad19a494bce7b34a6b4701636bae7510d1
zhushen <60245376+zhushen12580@users.noreply.github.com>
2023-09-22 11:57:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -119,23 +119,25 @@ def index():
         reviews_text = '\n'.join(reviews)
 
 
-        url = "http://120.79.81.153:8080/v1/chat/completions"
+        url = "url = "http://120.79.81.153/api/conversation/talk""
 
         headers = {
             "Content-Type": "application/json",
-            "Authorization":"TotallySecurePassword",
         }
 
         data = {
-            "model": "gpt-4",
-            "messages": [{"role": "user", "content": "作为nlp算法模型，分析以下产品信息及评论，找出该产品的用户需求点，格式要求：以分析报告的格式输出且是markdown格式的，层次清晰，重点突出，包含h1标题（产品设计优化方向分析报告）、小字体产品名（简称）、h2摘要、h2主要发现（每条发现标题加粗），主要发现下面增加一条引用的评论,并指出有多少条相似观点、h2结论与建议；产品信息及评论如下：产品信息："+reviews_text}],
+            "prompt": "作为nlp算法模型，分析以下产品信息及评论，找出该产品的用户需求点，格式要求：以分析报告的格式输出且是markdown格式的，层次清晰，重点突出，包含h1标题（产品设计优化方向分析报告）、小字体产品名（简称）、h2摘要、h2主要发现（每条发现标题加粗），主要发现下面增加一条引用的评论,并指出有多少条相似观点、h2结论与建议；产品信息及评论如下：产品信息："+reviews_text,
+            "model": "gpt-4-plugins",
+            "message_id": str(uuid.uuid4()),  # 自动生成UUID
+            "parent_message_id": str(uuid.uuid4()),  # 首次请求时生成新的UUID，之后可根据需要更新
+            "stream":False
         }
         print(reviews_text)
-        if len(reviews_text)<6500:
-            response = requests.post(url, json=data, headers=headers,timeout=4200)
+        if len(reviews_text)<10000:
+            response = requests.post(url, headers=headers, json=data,timeout=3000)
             if response.status_code == 200:
                 result = response.json()
-                results = result["choices"][0]["message"]["content"]
+                results =  response.json()['message']['content']['parts'][0]
                 print(result)
             else:
                 print("请求失败:", response.status_code, response.text)
