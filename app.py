f894746eb8a755b3052f2baf70d73e5c37d91770
zhushen <60245376+zhushen12580@users.noreply.github.com>
2023-10-12 02:31:42 +0000

    modified:   app.py 	modified:   requirements.txt

--- app.py
+++ app.py
@@ -43,6 +43,7 @@ def index():
 
             try:
                 url_detail1 = f"https://www.amazon.com/product-reviews/{asin}/ref=cm_cr_unknown?ie=UTF8&reviewerType=all_reviews&filterByStar=one_star&pageNumber=1"
+                print(url_detail1)
                 params = {
                     'token': token,
                     'scraper': 'amazon-product-reviews',
@@ -154,7 +155,6 @@ def index():
                 result = response.json()
                 results =  response.json()['message']['content']['parts'][0]
                 print(result)
-                return render_template('index.html', form=form, results=results,stars=product_review_top)
             else:
                 print("请求失败:", response.status_code, response.text)
         else:
@@ -162,7 +162,8 @@ def index():
             model="gpt-4-32k", messages=[{"role": "user", "content": "作为nlp算法模型，分析以下产品信息及评论，找出该产品的用户需求点，格式要求：以分析报告的格式输出且是markdown格式的，层次清晰，重点突出，包含h1标题（产品设计优化方向分析报告）、小字体产品名（简称）、h2摘要、h2主要发现（每条发现标题加粗），主要发现下面增加一条引用的评论,并指出有多少条相似观点、h2结论与建议；产品信息及评论如下：产品信息："+reviews_text}],timeout=3200.0)
             results = chat_completion.choices[0]['message']['content']
             print(results)
-            return render_template('index.html', form=form, results=results,stars=product_review_top)
+
+    return render_template('index.html', form=form, results=results,stars=product_review_top)
 
 
 if __name__ == '__main__':
